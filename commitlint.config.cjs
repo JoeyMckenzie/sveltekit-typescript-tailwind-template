636859f5120e35afc7d4d3e39d6c06af084d681a
Joey McKenzie <joey.mckenzie27@gmail.com>
2022-11-01 11:16:58 -0700

    feat(workspace): add commitlint

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -8,7 +8,6 @@ module.exports = {
 		}
 	},
 	rules: {
-		'references-empty': [2, 'never'],
 		'scope-empty': [1, 'never']
 	},
 	helpUrl: 'https://github.com/conventional-changelog/commitlint/#what-is-commitlint'
